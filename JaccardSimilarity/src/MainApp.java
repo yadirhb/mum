3feb09244358ccc083ecda2f23545d3b9ff1664b
unknown <610545@CS.MUM.EDU>
2019-09-21 11:15:01 -0500

    Modified the union definition to groupBy

--- JaccardSimilarity/src/MainApp.java
+++ JaccardSimilarity/src/MainApp.java
@@ -1,3 +1,6 @@
+import com.google.common.collect.Sets;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
@@ -7,22 +10,21 @@ public class MainApp {
     public static TetraFunction<List<PatientRecord<Medication>>, List<PatientRecord<Lab>>, List<PatientRecord<Diagnosis>>, Patient, Stream<PatientRecord>> getPatientRecords = (medications, labs, diagnosis, target) ->
             Stream.concat(
                     Stream.concat(
-                            medications.stream().filter(patientRecord -> patientRecord.getPatient().equals(target)),
-                            labs.stream().filter(patientRecord -> patientRecord.getPatient().equals(target))
+                            medications.stream().collect(Collectors.groupingBy(PatientRecord::getPatient)).get(target).stream(),
+                            labs.stream().collect(Collectors.groupingBy(PatientRecord::getPatient)).get(target).stream()
                     ),
-                    diagnosis.stream().filter(patientRecord -> patientRecord.getPatient().equals(target))
+                    diagnosis.stream().collect(Collectors.groupingBy(PatientRecord::getPatient)).get(target).stream()
             );
 
     public static BiFunction<Stream<PatientRecord>, Stream<PatientRecord>, Stream<PatientRecord>> intersect = (set1, set2) ->
-            set1.filter(
-                    patientRecord -> set2.anyMatch(patientRecord2 -> patientRecord.getRecord().equals(patientRecord.getRecord()))
-            );
+            Sets.intersection(new HashSet<>(set1.collect(Collectors.toList())),new HashSet<>(set2.collect(Collectors.toList()))).stream();
 
     public static BiFunction<Stream<PatientRecord>, Stream<PatientRecord>, Float> jaccardSimilarity = (set1, set2) ->
             Float.valueOf(intersect.apply(set1, set2).count() / (set1.count() + set2.count() - intersect.apply(set1, set2).count()));
 
     public static HexaFunction<List<Patient>, List<PatientRecord<Medication>>, List<PatientRecord<Lab>>, List<PatientRecord<Diagnosis>>, Integer, Patient, List<String>> topKMostSimilarPatients = (patients, medications, labs, diags, k, target) ->
             patients.stream()
+                    .filter(patient -> !patient.equals(target))
                     .sorted((patient1, patient2) ->
                             jaccardSimilarity.apply(
                                     getPatientRecords.apply(medications, labs, diags, patient2),
